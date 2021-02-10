5b988cfba2e971582a776809051db634d904c1a5
fajrifitriansyah <fajri.fitriansyah02@gmail.com>
2021-02-10 10:56:57 +0700

    menambah GitNotes.js

---
+++ GitNotes.js
@@ -0,0 +1,13 @@
+//  PERTAMAKALI YANG HARUS DILAKUKAN
+//  1. Initialize folder : git init
+//  2. Pilih filr yang akan di track :
+//                  - git add nama_file.ext
+//                  - git add .
+//  3. git commit -m "Notesnya apa???"
+//  4. git add remote origin link_repository
+//  5. git push origin master
+
+// ----------------------------------------
+//  git add
+//  git commit -m "Notesnya apa???"
+//  git push origin master
